03305ef960c0ea8f2c0ba2d9864e804821980790
dgallardo06 <denise.gallardo1@gmail.com>
2018-02-17 01:30:02 -0500

    prototypes to check letter and print output

--- word.js
+++ word.js
@@ -1,4 +1,4 @@
-var letter = require("./letter.js");
+var Letter = require("./letter.js");
 
 function Word(word){
 	this.word = word;
@@ -7,15 +7,47 @@ function Word(word){
 	//array to hold all letters guessed by user
 	this.guesses = [];
 
-	//function to go through each letter in word and add each to letters array
-	this.getLetters = function(){
-		for (var i = 0; i < this.word.length; i++){
-			var newLetter = new Letter(this.word[i]);
-			this.letters.push(newLetter);
+	//go through each letter in word and add each to letters array
+	for (var i = 0; i < this.word.length; i++){
+		var newLetter = new Letter(this.word[i]);
+		this.letters.push(newLetter);
+	}
+}
+
+Word.prototype.checkLetter = function(letter){
+	this.notCorrect = true;
+	this.isLetterValid = false;
+	var letter = letter.toLowerCase();
+	if (this.guesses.indexOf(letter) != -1){
+		this.isLetterValid = true;
+	} else {
+		this.guesses.push(letter);
+		for(var i = 0; i < this.letters.length; i++){
+			if (this.letters[i].letter.toLowerCase() == letter){
+				this.notCorrect = false;
+				this.letters[i].show = true;
+			}
 		}
 	}
 }
 
+Word.prototype.isComplete = function(){
+	for (var i = 0; i < this.letters.length; i++){
+		if (!this.letters[i].show){
+			return false;
+		}
+	}
+	return true;
+}
+
+Word.prototype.output = function(){
+	var output = "";
+	for (var i = 0; i < this.letters.length; i++){
+		output += this.letters[i].printInfo();
+	}
+
+	return output;
+}
 
 //export Word construct
 module.exports = Word;
