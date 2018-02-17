56dea707f3c21fd1371ad8faaa72cba2ea377faf
dgallardo06 <denise.gallardo1@gmail.com>
2018-02-17 07:52:27 -0500

    require words to guess

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-var word = require("./word.js");
-var letter = require("./letter.js");
+var Word = require("./word.js");
+var Letter = require("./letter.js");
+var wordsToGuess = require("./wordsToGuess.js");
 var inquirer = require("inquirer");
+var randomWord = game.randomWord;
+var guessesLeft = 10;
+var guessWord = new Word.Word(randomWord);
+
+
+function guessLetter(){
+	console.log(guessWord.toString());
+
+	inquirer.
+	
+}
